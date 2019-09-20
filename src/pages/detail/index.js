fb6cc7d9568dd3df2ab4c7698044143d5808a7e3
wozien <571455773@qq.com>
2019-09-20 16:38:14 +0800

    add

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -1,8 +1,28 @@
 import React, { Component } from 'react';
+import { DetailWrapper, Header, Content } from './style';
 
 class Detail extends Component {
   render() {
-    return <div>Detail</div>;
+    return (
+      <DetailWrapper>
+        <Header>衡水中学，被外地人占领的高考工厂</Header>
+        <Content>
+          <img
+            alt=""
+            src="//upload-images.jianshu.io/upload_images/3301720-db890fabf626e0ac.jpg?imageMogr2/auto-orient/strip|imageView2/2/w/1200/format/webp"
+          />
+          <p>
+            2017年，衡水中学考上清华北大的人数是176人，2016年是139人，再往前推到2015年，这个人数是119人。但是在这些辉煌的名单背后，却是外地来衡水上学人数暴涨，本地人上好高中越来越艰难的尴尬处境。
+          </p>
+          <p>
+            2017年，衡水中学考上清华北大的人数是176人，2016年是139人，再往前推到2015年，这个人数是119人。但是在这些辉煌的名单背后，却是外地来衡水上学人数暴涨，本地人上好高中越来越艰难的尴尬处境。
+          </p>
+          <p>
+            2017年，衡水中学考上清华北大的人数是176人，2016年是139人，再往前推到2015年，这个人数是119人。但是在这些辉煌的名单背后，却是外地来衡水上学人数暴涨，本地人上好高中越来越艰难的尴尬处境。
+          </p>
+        </Content>
+      </DetailWrapper>
+    );
   }
 }
 
